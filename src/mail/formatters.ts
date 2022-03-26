b249dec239a2dcb3f35df5bd778451bbfa7b800c
Lotte <lotteunckell@gmail.com>
2022-03-26 15:00:33 +0100

    fix the generation of tops for the mail invitation

--- src/mail/formatters.ts
+++ src/mail/formatters.ts
@@ -1,38 +1,31 @@
 import { Topic } from "../models/topic";
 
 export function generateTopicList(topics: Topic[]): string {
-    /*const mainTopics = topics.filter(t => !t.parent);
-    const subTopics = topics.filter(t => t.parent);
-    const sortedTopics = [];
-    for (const topic of mainTopics) {
-        sortedTopics.push(topic);
+    const mainTopics = topics.filter(t => !t.parent && t.position);
+    const nonMainTopics = topics.filter(t => t.parent && t.position);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const sortedTopics = mainTopics.sort((a, b) => (a.position > b.position) ? 1 : -1);
+    let message = "<ol>";
+    for (const topic of sortedTopics) {
+        message += `<li>${topic.name}`;
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        for (const subTopic of subTopics.filter(t => topic._id.equals(t.parent?._id)))
-            sortedTopics.push(subTopic);
+        const subTopics = nonMainTopics.filter(t => t.parent.id === topic.id );
+        if (subTopics.length > 0) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            const sortedSubTopics = subTopics.sort((a, b) => (a.position > b.position) ? 1 : -1);
+            message += "<ol>";
+            for (const subTopic of sortedSubTopics) {
+                message += `<li>${subTopic.name}</li>`;
+            }
+            message += "</ol></li>";
+        } else message += "</li>";
     }
-
-    let message = "<ol><li>";
-    let isIndented = false;
-    for (const topic of sortedTopics) {
-        if (topic.parent) {
-            if (!isIndented) {
-                message += "<ol><li>";
-                isIndented = true;
-            } else message += "</li><li>";
-            message += `${topic.fullPosition} ${topic.name}`;
-        } else {
-            if (isIndented) {
-                message += "</li></ol><li>";
-                isIndented = false;
-            } else message += "</li><li>";
-            message += `${topic.fullPosition} ${topic.name}`;
-        }
-    }
-    message += "</li></ol>";
-    return message;*/
-    // TODO: Fix this
-    return "to be fixed";
+    message += "</ol>";
+    console.log(message);
+    return message;
 }
 
 const longDateTimeFormat = new Intl.DateTimeFormat("de-de", {
